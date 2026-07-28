fb8cac0de8f9e303a1a5bc13cf8f534800b83b7c
LightMind Dev <dev@lightmindlab.com>
2026-07-28 11:01:20 +0000

    feat: configure custom domain docs.lightmind.top

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'LightMind Docs',
   description: 'LightMind 项目官方文档',
-  base: '/lightminddocs/',
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
